a04d3df9fa2e1a8385732da03e9fe3ea29b80726
SubFabula <150776952+SubFabula@users.noreply.github.com>
2024-02-18 19:21:29 +0300

    Trouble Shooting Background Toggle

--- JavaScript/script-light_and_dark_toggle.js
+++ JavaScript/script-light_and_dark_toggle.js
@@ -1,6 +1,6 @@
 function toggleMode() {
     var stylesheet = document.getElementById('stylesheet');
-    if (stylesheet.href.includes('styles-light.css')) {
+    if (stylesheet.href.includes('CSS/styles-light.css')) {
         stylesheet.href = 'CSS/styles-dark.css'; // Switch to dark mode
     } else {
         stylesheet.href = 'CSS/styles-light.css'; // Switch to light mode
