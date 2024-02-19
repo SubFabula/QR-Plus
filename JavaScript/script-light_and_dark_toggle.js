71479db1fbac3eee5d1ca06da84535fdbda7528b
SubFabula <150776952+SubFabula@users.noreply.github.com>
2024-02-19 22:18:18 +0300

    Trouble Shooting PNG Background

--- JavaScript/script-light_and_dark_toggle.js
+++ JavaScript/script-light_and_dark_toggle.js
@@ -1,12 +1,21 @@
+// script-light_and_dark_toggle.js
+
 function toggleMode() {
     var body = document.body;
-    if (body.classList.contains('dark-mode')) {
-        // Switch to light mode
-        body.classList.remove('dark-mode');
-        body.classList.add('light-mode');
-    } else {
+    var themeStylesheet = document.getElementById('themeStylesheet');
+    var isLightMode = themeStylesheet.href.includes('styles-light-png.css');
+
+    // Toggle between light and dark mode classes on the body element
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+
+    // Toggle between light and dark mode stylesheets
+    if (isLightMode) {
         // Switch to dark mode
-        body.classList.remove('light-mode');
-        body.classList.add('dark-mode');
+        themeStylesheet.href = 'CSS/styles-dark-png.css';
+    } else {
+        // Switch to light mode
+        themeStylesheet.href = 'CSS/styles-light-png.css';
     }
 }
+
