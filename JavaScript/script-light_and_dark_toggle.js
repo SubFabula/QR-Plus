3a842b3b67cd2e30dedbd6c5cb98c9302ee08acc
SubFabula <150776952+SubFabula@users.noreply.github.com>
2024-02-19 21:57:24 +0300

    PNG Background

--- JavaScript/script-light_and_dark_toggle.js
+++ JavaScript/script-light_and_dark_toggle.js
@@ -1,4 +1,12 @@
 function toggleMode() {
     var body = document.body;
-    body.classList.toggle('light-mode'); // Toggle light mode class on body
+    if (body.classList.contains('dark-mode')) {
+        // Switch to light mode
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+    } else {
+        // Switch to dark mode
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+    }
 }
